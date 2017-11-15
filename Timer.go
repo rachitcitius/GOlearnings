d18791d68e5a461168b116c0490ec5e9ded8548e
rachitkohli <32201739+rachitkohli@users.noreply.github.com>
2017-11-15 18:26:12 +0530

    Add files via upload

--- Timer.go
+++ Timer.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	// TimerStop()
-	TickerStart()
+	// TickerStart()
+	WorkerPool()
 }
 
 func TimerStop() {
@@ -42,4 +43,35 @@ func TickerStart() {
 	time.Sleep(time.Millisecond * 1600)
 	ticker.Stop()
 	fmt.Println("Ticker Stopped!")
+}
+
+func WorkerPool() {
+	//creating worker pool 
+	worker := func(id int, jobs<-chan int, results chan<-int) {
+		for j:=range jobs {
+			fmt.Println("Worker", id, "started job", j)
+			time.Sleep(time.Second)
+			fmt.Println("Worker", id, "finished job", j)
+			results <- j*2
+		}
+	}
+	
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+	
+	for w:=1;w<=3;w++ {
+		//getting workers to start working
+		go worker(w, jobs, results)
+	}
+	
+	for i:=1;i<=5;i++ {
+		//Sending job
+		jobs <- i * 10
+	}
+	close(jobs)
+	
+	for a:=1;a<=5;a++ {
+		//collecting results
+		<-results
+	}
 }
