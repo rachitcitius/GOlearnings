f12a4ae0653aac27e322acf68733b0d1dc11f59a
rachitkohli <32201739+rachitkohli@users.noreply.github.com>
2017-11-16 18:34:54 +0530

    Updated with goroutines & channels

--- ManagingState.go
+++ ManagingState.go
@@ -12,7 +12,8 @@ import (
 
 func main() {
 	// UsingAtomicCounter()
-	UsingMutex()
+	// UsingMutex()
+	UsingStateful_GoRoutines()
 }
 
 func UsingAtomicCounter() {
@@ -84,4 +85,74 @@ func UsingMutex() {
 	mutex.Lock()
 	fmt.Println("State:", state)
 	mutex.Unlock()
+}
+
+func UsingStateful_GoRoutines() {
+	//We will preserve the state using the inbuilt syncronization features of GoRoutines & Channels
+	
+//Creating structures to maintain the read & write
+type readOpSt struct {	
+	key int
+	resp chan int
+}
+
+type writeOpSt struct {
+	key int
+	value int
+	resp chan bool
+}
+
+//Read/Write channels
+readCh := make(chan *readOpSt)
+writeCh := make(chan *writeOpSt)
+
+//Read/Write counters
+var readOps, writeOps uint64 = 0, 0
+
+go func() {
+	//state map to store values. This is private to current goroutine
+	var state = make(map[int]int)
+	for {
+		select {
+			case read := <-readCh:
+				read.resp <- state[read.key]
+				
+			case write := <-writeCh:
+				state[write.key] = write.value	//writing back to the state
+				write.resp <- true	//successfully written
+				
+		}
+	}
+}()
+
+//Starting 100 goroutines to read the state via the readCh channel
+for r:=0; r<100; r++ {
+	go func() {
+		for {
+			read := &readOpSt{key: rand.Intn(5),  resp: make(chan int) }	
+			readCh <- read
+			<- read.resp
+			atomic.AddUint64(&readOps, 1)	//Counter
+			time.Sleep(time.Millisecond)
+		}
+	}()
+}
+
+for w:=0; w<10; w++ {
+	go func() {
+		for {
+			write := &writeOpSt{key: rand.Intn(5), value: rand.Intn(100), resp: make(chan bool)}
+			writeCh <- write
+			<-write.resp
+			atomic.AddUint64(&writeOps, 1)
+			time.Sleep(time.Millisecond)
+		}
+	}()
+}
+
+time.Sleep(time.Second)
+
+fmt.Println("Read Ops:", readOps)
+fmt.Println("Write Ops:", writeOps)
+
 }
