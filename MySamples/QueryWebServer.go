2f533dba1c739b6b8908ddcf69433755400434b9
rachitkohli <32201739+rachitkohli@users.noreply.github.com>
2017-11-20 20:13:00 +0530

    Updated code by using channels
    Receiving the responses over a channel & reading them

--- MySamples/QueryWebServer.go
+++ MySamples/QueryWebServer.go
@@ -17,8 +17,12 @@ type apiStruc struct {
 
 //Global map variable to store the individual response
 var finalResponse map[string]string
+var counter int
+var responseChan chan string
+const ApiCounts=50
 
 func main() {
+	responseChan = make(chan string, ApiCounts)	//Channel to receive response
 	finalResponse = make(map[string]string)
 	http.HandleFunc("/", APIcall)
 	http.ListenAndServe(":1234", nil)	
@@ -26,16 +30,29 @@ func main() {
 
 func APIcall(w http.ResponseWriter, r *http.Request) {
 	r.Header.Set("Content-Type", "text/html")
-	for i:=1;i<=20;i++ {
-		//Calling the API concurrently
+	counter = 0
+	for i:=1;i<=ApiCounts;i++ {
+		//Calling the API concurrently by making that number of Goroutines
 		go QueryServer(strconv.Itoa(i))	
 	}	
 	time.Sleep(time.Second * 2)
+	
+	for j:=1;j<=ApiCounts;j++ {
+		w.Write([]byte("<div>"))
+		select {
+			case response := <-responseChan:	//Receiving on the channel
+				w.Write([]byte( strconv.Itoa(j) + " -> " + response + "</br>"))
+		}
+	}
+	
+	/*
+	//Writing back to the browser
 	w.Write([]byte("<div>"))
 	for a:=range finalResponse {
 		w.Write([]byte( a + "->" + finalResponse[a] + "</br>"))
 	}
 	w.Write([]byte("</div>"))
+	*/
 }
 
 func QueryServer(s string) {
@@ -49,4 +66,7 @@ func QueryServer(s string) {
 	var apiS apiStruc
 	json.NewDecoder(resp.Body).Decode(&apiS)
 	finalResponse[s] = fmt.Sprint(apiS)	//Appending the response in map
+	responseChan <- finalResponse[s]	//Sending to the channel
+	counter += 1
+	fmt.Println("Received ", counter)
 }
